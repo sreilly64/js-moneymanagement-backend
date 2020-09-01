152c4bf57c24a7bd1b296cd4e7df5473f70047b3
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-09-01 16:21:24 -0400

    add spring boot

---
+++ src/main/java/com/jszipcoders/moneymanager/MoneymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.jszipcoders.moneymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymanagerApplication.class, args);
+	}
+
+}
