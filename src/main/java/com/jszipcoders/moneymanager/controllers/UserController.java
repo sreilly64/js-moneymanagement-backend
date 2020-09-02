cf6f1a5b92839f46b33f9b0c35e84626f45b6ca2
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-09-02 18:14:41 -0400

    Filled out fields and methods for User classes

--- src/main/java/com/jszipcoders/moneymanager/controllers/UserController.java
+++ src/main/java/com/jszipcoders/moneymanager/controllers/UserController.java
@@ -1,10 +1,22 @@
 package com.jszipcoders.moneymanager.controllers;
 
+import com.jszipcoders.moneymanager.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/api")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 
 }
