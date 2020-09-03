4fa52dbbc2b865b6190ee6cb56b76115cc1bed84
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-09-03 14:40:37 -0400

    add CRUD operations to User

--- src/main/java/com/jszipcoders/moneymanager/services/UserService.java
+++ src/main/java/com/jszipcoders/moneymanager/services/UserService.java
@@ -1,5 +1,6 @@
 package com.jszipcoders.moneymanager.services;
 
+import com.jszipcoders.moneymanager.entities.UserEntity;
 import com.jszipcoders.moneymanager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,29 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepo){
         this.userRepo = userRepo;
+    }
+
+    public UserEntity addUser(UserEntity newUser) {
+        return userRepo.save(newUser);
+    }
+
+    public UserEntity findUserById(Long user_id) {
+        return userRepo.findById(user_id).get();
+    }
+
+    public UserEntity updateUserDetails(Long user_id, UserEntity updatedUser) {
+        UserEntity userEntity = userRepo.findById(user_id).get();
+        if(userEntity != null){
+            userEntity.setFirstName(updatedUser.getFirstName());
+            userEntity.setLastName(updatedUser.getLastName());
+            userEntity.setAddress(updatedUser.getAddress());
+            userEntity.setEmail(updatedUser.getEmail());
+            userEntity.setPhoneNumber(updatedUser.getPhoneNumber());
+            userEntity.setPassword(updatedUser.getPassword());
+            userEntity.setAccounts(updatedUser.getAccounts());
+            return userRepo.save(userEntity);
+        }else{
+            return userEntity;
+        }
     }
 }
