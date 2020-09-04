a455754bf4cef1314ded7f8b32c644f95d969b21
Julie Moon <juliemoon95@gmail.com>
2020-09-04 00:45:18 -0400

    adding properties

--- src/main/java/com/jszipcoders/moneymanager/MoneyManagerApplication.java
+++ src/main/java/com/jszipcoders/moneymanager/MoneyManagerApplication.java
@@ -2,8 +2,10 @@ package com.jszipcoders.moneymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class MoneyManagerApplication {
 
 	public static void main(String[] args) {
