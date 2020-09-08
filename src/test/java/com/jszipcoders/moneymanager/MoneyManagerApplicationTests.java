0cc8206456d0d64d4ee4b40a1866a45ff67f715a
Julie Moon <juliemoon95@gmail.com>
2020-09-08 12:20:24 -0400

    added Test for Main App

--- src/test/java/com/jszipcoders/moneymanager/MoneyManagerApplicationTests.java
+++ src/test/java/com/jszipcoders/moneymanager/MoneyManagerApplicationTests.java
@@ -1,13 +1,16 @@
-//package com.jszipcoders.moneymanager;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class MoneyManagerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.jszipcoders.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MoneyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
