e7631e33ab6b012c10e19a85ba394e324be79033
Julie Moon <juliemoon95@gmail.com>
2020-09-23 13:51:18 -0400

    added data config

---
+++ src/main/java/com/jszipcoders/moneymanager/config/DataSourceConfiguration.java
@@ -0,0 +1,30 @@
+package com.jszipcoders.moneymanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("cloud")
+public class DataSourceConfiguration {
+
+    private static final String USER_NAME = System.getenv("DB_UN");
+    private static final String PASSWORD = System.getenv("DB_PW");
+    private static final String URL = System.getenv("DB_URL");
+
+    @Bean
+    public DataSource dataSource() {
+
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setUsername(USER_NAME);
+        hikariConfig.setPassword(PASSWORD);
+        hikariConfig.setJdbcUrl(URL);
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+
+        return new HikariDataSource(hikariConfig);
+    }
+}
