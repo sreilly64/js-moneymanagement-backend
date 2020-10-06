c834a04c42ab3edca866e9d1f26b5960f8227102
shane <sreilly@udel.edu>
2020-10-06 15:30:27 -0400

    configured h2 database for local testing of new nickname feature

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,6 +1,6 @@
-INSERT INTO USERENTITY (firstName, lastName, ssn, address, email, phoneNumber, username, password)
-VALUES ('Julie', 'Moon', '123-45-6789', '123 Senior St. Wilmington, DE, 19803', 'juliemoon@gmail.com', '201-236-8739', 'julie185', 'supersecret');
+INSERT INTO USERENTITY (userId, firstName, lastName, ssn, address, email, phoneNumber, username, password)
+VALUES (1, 'Shane', 'Reilly', '123-45-6789', '123 Street Rd, Wilmington, DE 19803', 'shane@gmail.com', '302-555-5555', 'Codingrulez67', 'Shm00ples!');
 
-INSERT INTO ACCOUNTENTITY (type, routingNumber, balance)
-VALUES ('CHECKING', 394058927, 2000.00);
+INSERT INTO ACCOUNTENTITY (accountNumber, type, routingNumber, balance, userId)
+VALUES (100, 1, 394058927, 2000.00, 1);
 
